5a36e618d2ceeba2bbd8e556832560f0e5eadcec
Bryce Madelo <brycemadelo07@gmail.com>
2022-06-09 10:44:21 +0800

    Added the function to the rest of the button.

--- NewTicTacToe/Form1.Designer.cs
+++ NewTicTacToe/Form1.Designer.cs
@@ -43,7 +43,7 @@
             // btnBoard01
             // 
             this.btnBoard01.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnBoard01.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBoard01.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBoard01.Location = new System.Drawing.Point(33, 80);
             this.btnBoard01.Name = "btnBoard01";
             this.btnBoard01.Size = new System.Drawing.Size(62, 54);
@@ -54,74 +54,90 @@
             // btnBoard02
             // 
             this.btnBoard02.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBoard02.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBoard02.Location = new System.Drawing.Point(101, 80);
             this.btnBoard02.Name = "btnBoard02";
             this.btnBoard02.Size = new System.Drawing.Size(62, 54);
             this.btnBoard02.TabIndex = 4;
             this.btnBoard02.UseVisualStyleBackColor = true;
+            this.btnBoard02.Click += new System.EventHandler(this.btnBoard02_Click);
             // 
             // btnBoard03
             // 
             this.btnBoard03.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBoard03.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBoard03.Location = new System.Drawing.Point(169, 80);
             this.btnBoard03.Name = "btnBoard03";
             this.btnBoard03.Size = new System.Drawing.Size(62, 54);
             this.btnBoard03.TabIndex = 5;
             this.btnBoard03.UseVisualStyleBackColor = true;
+            this.btnBoard03.Click += new System.EventHandler(this.btnBoard03_Click);
             // 
             // btnBoard04
             // 
             this.btnBoard04.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBoard04.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBoard04.Location = new System.Drawing.Point(33, 140);
             this.btnBoard04.Name = "btnBoard04";
             this.btnBoard04.Size = new System.Drawing.Size(62, 54);
             this.btnBoard04.TabIndex = 6;
             this.btnBoard04.UseVisualStyleBackColor = true;
+            this.btnBoard04.Click += new System.EventHandler(this.btnBoard04_Click);
             // 
             // btnBoard05
             // 
             this.btnBoard05.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBoard05.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBoard05.Location = new System.Drawing.Point(101, 140);
             this.btnBoard05.Name = "btnBoard05";
             this.btnBoard05.Size = new System.Drawing.Size(62, 54);
             this.btnBoard05.TabIndex = 7;
             this.btnBoard05.UseVisualStyleBackColor = true;
+            this.btnBoard05.Click += new System.EventHandler(this.btnBoard05_Click);
             // 
             // btnBoard06
             // 
             this.btnBoard06.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBoard06.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBoard06.Location = new System.Drawing.Point(169, 140);
             this.btnBoard06.Name = "btnBoard06";
             this.btnBoard06.Size = new System.Drawing.Size(62, 54);
             this.btnBoard06.TabIndex = 8;
             this.btnBoard06.UseVisualStyleBackColor = true;
+            this.btnBoard06.Click += new System.EventHandler(this.btnBoard06_Click);
             // 
             // btnBoard07
             // 
             this.btnBoard07.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBoard07.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBoard07.Location = new System.Drawing.Point(33, 200);
             this.btnBoard07.Name = "btnBoard07";
             this.btnBoard07.Size = new System.Drawing.Size(62, 54);
             this.btnBoard07.TabIndex = 9;
             this.btnBoard07.UseVisualStyleBackColor = true;
+            this.btnBoard07.Click += new System.EventHandler(this.btnBoard07_Click);
             // 
             // btnBoard08
             // 
             this.btnBoard08.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBoard08.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBoard08.Location = new System.Drawing.Point(101, 200);
             this.btnBoard08.Name = "btnBoard08";
             this.btnBoard08.Size = new System.Drawing.Size(62, 54);
             this.btnBoard08.TabIndex = 10;
             this.btnBoard08.UseVisualStyleBackColor = true;
+            this.btnBoard08.Click += new System.EventHandler(this.btnBoard08_Click);
             // 
             // btnBoard09
             // 
             this.btnBoard09.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBoard09.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBoard09.Location = new System.Drawing.Point(169, 200);
             this.btnBoard09.Name = "btnBoard09";
             this.btnBoard09.Size = new System.Drawing.Size(62, 54);
             this.btnBoard09.TabIndex = 11;
             this.btnBoard09.UseVisualStyleBackColor = true;
+            this.btnBoard09.Click += new System.EventHandler(this.btnBoard09_Click);
             // 
             // label1
             // 
--- NewTicTacToe/Form1.cs
+++ NewTicTacToe/Form1.cs
@@ -33,5 +33,133 @@ namespace NewTicTacToe
                 btnBoard01.Text = "O"; btnBoard01.Enabled = false;
             }
         }
+
+        private void btnBoard02_Click(object sender, EventArgs e)
+        {
+            if (opponent)
+            {
+                btnBoard02.Text = "X";
+                opponent = !opponent;
+                btnBoard02.Text = "X"; btnBoard02.Enabled = false;
+            }
+            else
+            {
+                btnBoard02.Text = "O";
+                opponent = !opponent;
+                btnBoard02.Text = "O"; btnBoard02.Enabled = false;
+            }
+        }
+
+        private void btnBoard03_Click(object sender, EventArgs e)
+        {
+            if (opponent)
+            {
+                btnBoard03.Text = "X";
+                opponent = !opponent;
+                btnBoard03.Text = "X"; btnBoard03.Enabled = false;
+            }
+            else
+            {
+                btnBoard03.Text = "O";
+                opponent = !opponent;
+                btnBoard03.Text = "O"; btnBoard03.Enabled = false;
+            }
+        }
+
+        private void btnBoard04_Click(object sender, EventArgs e)
+        {
+            if (opponent)
+            {
+                btnBoard04.Text = "X";
+                opponent = !opponent;
+                btnBoard04.Text = "X"; btnBoard04.Enabled = false;
+            }
+            else
+            {
+                btnBoard04.Text = "O";
+                opponent = !opponent;
+                btnBoard04.Text = "O"; btnBoard04.Enabled = false;
+            }
+        }
+
+        private void btnBoard05_Click(object sender, EventArgs e)
+        {
+            if (opponent)
+            {
+                btnBoard05.Text = "X";
+                opponent = !opponent;
+                btnBoard05.Text = "X"; btnBoard05.Enabled = false;
+            }
+            else
+            {
+                btnBoard05.Text = "O";
+                opponent = !opponent;
+                btnBoard05.Text = "O"; btnBoard05.Enabled = false;
+            }
+        }
+
+        private void btnBoard06_Click(object sender, EventArgs e)
+        {
+            if (opponent)
+            {
+                btnBoard06.Text = "X";
+                opponent = !opponent;
+                btnBoard06.Text = "X"; btnBoard06.Enabled = false;
+            }
+            else
+            {
+                btnBoard06.Text = "O";
+                opponent = !opponent;
+                btnBoard06.Text = "O"; btnBoard06.Enabled = false;
+            }
+        }
+
+        private void btnBoard07_Click(object sender, EventArgs e)
+        {
+            if (opponent)
+            {
+                btnBoard07.Text = "X";
+                opponent = !opponent;
+                btnBoard07.Text = "X"; btnBoard07.Enabled = false;
+            }
+            else
+            {
+                btnBoard07.Text = "O";
+                opponent = !opponent;
+                btnBoard07.Text = "O"; btnBoard07.Enabled = false;
+            }
+        }
+
+        private void btnBoard08_Click(object sender, EventArgs e)
+        {
+            if (opponent)
+            {
+                btnBoard08.Text = "X";
+                opponent = !opponent;
+                btnBoard08.Text = "X"; btnBoard08.Enabled = false;
+            }
+            else
+            {
+                btnBoard08.Text = "O";
+                opponent = !opponent;
+                btnBoard08.Text = "O"; btnBoard08.Enabled = false;
+            }
+        }
+
+        private void btnBoard09_Click(object sender, EventArgs e)
+        {
+            if (opponent)
+            {
+                btnBoard09.Text = "X";
+                opponent = !opponent;
+                btnBoard09.Text = "X"; btnBoard09.Enabled = false;
+            }
+            else
+            {
+                btnBoard09.Text = "O";
+                opponent = !opponent;
+                btnBoard09.Text = "O"; btnBoard09.Enabled = false;
+            }
+        }
     }
 }
