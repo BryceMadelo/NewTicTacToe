aa27be1083150eeed1343401b32e36256875697f
Bryce Madelo <brycemadelo07@gmail.com>
2022-06-09 09:01:52 +0800

    Start

---
+++ NewTicTacToe/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace NewTicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBoard01 = new System.Windows.Forms.Button();
+            this.btnBoard02 = new System.Windows.Forms.Button();
+            this.btnBoard03 = new System.Windows.Forms.Button();
+            this.btnBoard04 = new System.Windows.Forms.Button();
+            this.btnBoard05 = new System.Windows.Forms.Button();
+            this.btnBoard06 = new System.Windows.Forms.Button();
+            this.btnBoard07 = new System.Windows.Forms.Button();
+            this.btnBoard08 = new System.Windows.Forms.Button();
+            this.btnBoard09 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBoard01
+            // 
+            this.btnBoard01.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBoard01.Location = new System.Drawing.Point(33, 80);
+            this.btnBoard01.Name = "btnBoard01";
+            this.btnBoard01.Size = new System.Drawing.Size(62, 54);
+            this.btnBoard01.TabIndex = 3;
+            this.btnBoard01.UseVisualStyleBackColor = true;
+            // 
+            // btnBoard02
+            // 
+            this.btnBoard02.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBoard02.Location = new System.Drawing.Point(101, 80);
+            this.btnBoard02.Name = "btnBoard02";
+            this.btnBoard02.Size = new System.Drawing.Size(62, 54);
+            this.btnBoard02.TabIndex = 4;
+            this.btnBoard02.UseVisualStyleBackColor = true;
+            // 
+            // btnBoard03
+            // 
+            this.btnBoard03.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBoard03.Location = new System.Drawing.Point(169, 80);
+            this.btnBoard03.Name = "btnBoard03";
+            this.btnBoard03.Size = new System.Drawing.Size(62, 54);
+            this.btnBoard03.TabIndex = 5;
+            this.btnBoard03.UseVisualStyleBackColor = true;
+            // 
+            // btnBoard04
+            // 
+            this.btnBoard04.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBoard04.Location = new System.Drawing.Point(33, 140);
+            this.btnBoard04.Name = "btnBoard04";
+            this.btnBoard04.Size = new System.Drawing.Size(62, 54);
+            this.btnBoard04.TabIndex = 6;
+            this.btnBoard04.UseVisualStyleBackColor = true;
+            // 
+            // btnBoard05
+            // 
+            this.btnBoard05.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBoard05.Location = new System.Drawing.Point(101, 140);
+            this.btnBoard05.Name = "btnBoard05";
+            this.btnBoard05.Size = new System.Drawing.Size(62, 54);
+            this.btnBoard05.TabIndex = 7;
+            this.btnBoard05.UseVisualStyleBackColor = true;
+            // 
+            // btnBoard06
+            // 
+            this.btnBoard06.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBoard06.Location = new System.Drawing.Point(169, 140);
+            this.btnBoard06.Name = "btnBoard06";
+            this.btnBoard06.Size = new System.Drawing.Size(62, 54);
+            this.btnBoard06.TabIndex = 8;
+            this.btnBoard06.UseVisualStyleBackColor = true;
+            // 
+            // btnBoard07
+            // 
+            this.btnBoard07.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBoard07.Location = new System.Drawing.Point(33, 200);
+            this.btnBoard07.Name = "btnBoard07";
+            this.btnBoard07.Size = new System.Drawing.Size(62, 54);
+            this.btnBoard07.TabIndex = 9;
+            this.btnBoard07.UseVisualStyleBackColor = true;
+            // 
+            // btnBoard08
+            // 
+            this.btnBoard08.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBoard08.Location = new System.Drawing.Point(101, 200);
+            this.btnBoard08.Name = "btnBoard08";
+            this.btnBoard08.Size = new System.Drawing.Size(62, 54);
+            this.btnBoard08.TabIndex = 10;
+            this.btnBoard08.UseVisualStyleBackColor = true;
+            // 
+            // btnBoard09
+            // 
+            this.btnBoard09.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBoard09.Location = new System.Drawing.Point(169, 200);
+            this.btnBoard09.Name = "btnBoard09";
+            this.btnBoard09.Size = new System.Drawing.Size(62, 54);
+            this.btnBoard09.TabIndex = 11;
+            this.btnBoard09.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Schoolbook", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 30);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "TIC TAC TOE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 277);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBoard09);
+            this.Controls.Add(this.btnBoard08);
+            this.Controls.Add(this.btnBoard07);
+            this.Controls.Add(this.btnBoard06);
+            this.Controls.Add(this.btnBoard05);
+            this.Controls.Add(this.btnBoard04);
+            this.Controls.Add(this.btnBoard03);
+            this.Controls.Add(this.btnBoard02);
+            this.Controls.Add(this.btnBoard01);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnBoard01;
+        private System.Windows.Forms.Button btnBoard02;
+        private System.Windows.Forms.Button btnBoard03;
+        private System.Windows.Forms.Button btnBoard04;
+        private System.Windows.Forms.Button btnBoard05;
+        private System.Windows.Forms.Button btnBoard06;
+        private System.Windows.Forms.Button btnBoard07;
+        private System.Windows.Forms.Button btnBoard08;
+        private System.Windows.Forms.Button btnBoard09;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
