e1a541c6d28e25234bf84ce4ed7efc14c27ead02
Bryce Madelo <brycemadelo07@gmail.com>
2022-06-12 18:51:27 +0800

    Added move counts for the draw checker.

--- NewTicTacToe/Form1.cs
+++ NewTicTacToe/Form1.cs
@@ -13,25 +13,35 @@ namespace NewTicTacToe
     public partial class Form1 : Form
     {
         bool opponent = true; //opponent is used as variable for players
+        int opponent_count = 0;
         public Form1()
         {
             InitializeComponent();
         }
-
+        private void DrawChecker()//Checks the draw
+        {
+            if (opponent_count == 9)
+            {
+                MessageBox.Show("The game is draw! Please press RESTART to play again:)");
+            }
+        }
         private void btnBoard01_Click(object sender, EventArgs e)
         {
             if (opponent)
             {
                 btnBoard01.Text = "X";
                 opponent = !opponent;
+                opponent_count++;
                 btnBoard01.Text = "X"; btnBoard01.Enabled = false;
             }
             else
             {
                 btnBoard01.Text = "O";
                 opponent = !opponent;
+                opponent_count++;
                 btnBoard01.Text = "O"; btnBoard01.Enabled = false;
             }
+            DrawChecker();
             //Checking of Winner for 1st button
             //Winner in Horizontal = btnBoard 01, 02, and 03.
             if (btnBoard01.Text == "X" && btnBoard02.Text == "X" && btnBoard03.Text == "X")
@@ -115,21 +125,23 @@ namespace NewTicTacToe
                 btnBoard09.Enabled = false;
             }
         }
-
         private void btnBoard02_Click(object sender, EventArgs e)
         {
             if (opponent)
             {
                 btnBoard02.Text = "X";
                 opponent = !opponent;
+                opponent_count++;
                 btnBoard02.Text = "X"; btnBoard02.Enabled = false;
             }
             else
             {
                 btnBoard02.Text = "O";
                 opponent = !opponent;
+                opponent_count++;
                 btnBoard02.Text = "O"; btnBoard02.Enabled = false;
             }
+            DrawChecker();
             //Checking of Winner for 2nd button
             //Winner in Horizontal = btnBoard 01, 02, 03
             if (btnBoard01.Text == "X" && btnBoard02.Text == "X" && btnBoard03.Text == "X")
@@ -186,21 +198,23 @@ namespace NewTicTacToe
                 btnBoard09.Enabled = false;
             }
         }
-
         private void btnBoard03_Click(object sender, EventArgs e)
         {
             if (opponent)
             {
                 btnBoard03.Text = "X";
                 opponent = !opponent;
+                opponent_count++;
                 btnBoard03.Text = "X"; btnBoard03.Enabled = false;
             }
             else
             {
                 btnBoard03.Text = "O";
                 opponent = !opponent;
+                opponent_count++;
                 btnBoard03.Text = "O"; btnBoard03.Enabled = false;
             }
+            DrawChecker();
             //Checking of Winner for 3rd button
             //Winner in Horizontal = btnBoard 01, 02, 03
             if (btnBoard01.Text == "X" && btnBoard02.Text == "X" && btnBoard03.Text == "X")
@@ -284,21 +298,23 @@ namespace NewTicTacToe
                 btnBoard09.Enabled = false;
             }
         }
-
         private void btnBoard04_Click(object sender, EventArgs e)
         {
             if (opponent)
             {
                 btnBoard04.Text = "X";
                 opponent = !opponent;
+                opponent_count++;
                 btnBoard04.Text = "X"; btnBoard04.Enabled = false;
             }
             else
             {
                 btnBoard04.Text = "O";
                 opponent = !opponent;
+                opponent_count++;
                 btnBoard04.Text = "O"; btnBoard04.Enabled = false;
             }
+            DrawChecker();
             //Checking of Winner for 4th button
             //Winner in Horizontal = btnBoard 04, 05, 06
             if (btnBoard04.Text == "X" && btnBoard05.Text == "X" && btnBoard06.Text == "X")
@@ -355,21 +371,23 @@ namespace NewTicTacToe
                 btnBoard09.Enabled = false;
             }
         }
-
         private void btnBoard05_Click(object sender, EventArgs e)
         {
             if (opponent)
             {
                 btnBoard05.Text = "X";
                 opponent = !opponent;
+                opponent_count++;
                 btnBoard05.Text = "X"; btnBoard05.Enabled = false;
             }
             else
             {
                 btnBoard05.Text = "O";
                 opponent = !opponent;
+                opponent_count++;
                 btnBoard05.Text = "O"; btnBoard05.Enabled = false;
             }
+            DrawChecker();
             //Checking of Winner for 5th button
             //Winner in Vertical = btnBoard 02, 05, 08
             if (btnBoard02.Text == "X" && btnBoard05.Text == "X" && btnBoard08.Text == "X")
@@ -453,21 +471,23 @@ namespace NewTicTacToe
                 btnBoard09.Enabled = false;
             }
         }
-
         private void btnBoard06_Click(object sender, EventArgs e)
         {
             if (opponent)
             {
                 btnBoard06.Text = "X";
                 opponent = !opponent;
+                opponent_count++;
                 btnBoard06.Text = "X"; btnBoard06.Enabled = false;
             }
             else
             {
                 btnBoard06.Text = "O";
                 opponent = !opponent;
+                opponent_count++;
                 btnBoard06.Text = "O"; btnBoard06.Enabled = false;
             }
+            DrawChecker();
             //Checking of Winner for 6th button
             //Winner in Vertical = btnBoard 03, 06, 09
             if (btnBoard03.Text == "X" && btnBoard06.Text == "X" && btnBoard09.Text == "X")
@@ -524,21 +544,23 @@ namespace NewTicTacToe
                 btnBoard09.Enabled = false;
             }
         }
-
         private void btnBoard07_Click(object sender, EventArgs e)
         {
             if (opponent)
             {
                 btnBoard07.Text = "X";
                 opponent = !opponent;
+                opponent_count++;
                 btnBoard07.Text = "X"; btnBoard07.Enabled = false;
             }
             else
             {
                 btnBoard07.Text = "O";
                 opponent = !opponent;
+                opponent_count++;
                 btnBoard07.Text = "O"; btnBoard07.Enabled = false;
             }
+            DrawChecker();
             //Checking of Winner for 7th button
             //Winner in Vertical = btnBoard 01, 04, 07
             if (btnBoard01.Text == "X" && btnBoard04.Text == "X" && btnBoard07.Text == "X")
@@ -622,21 +644,23 @@ namespace NewTicTacToe
                 btnBoard09.Enabled = false;
             }
         }
-
         private void btnBoard08_Click(object sender, EventArgs e)
         {
             if (opponent)
             {
                 btnBoard08.Text = "X";
                 opponent = !opponent;
+                opponent_count++;
                 btnBoard08.Text = "X"; btnBoard08.Enabled = false;
             }
             else
             {
                 btnBoard08.Text = "O";
                 opponent = !opponent;
+                opponent_count++;
                 btnBoard08.Text = "O"; btnBoard08.Enabled = false;
             }
+            DrawChecker();
             //Checking of Winner for 8th button
             //Winner in Horizontal = btnBoard 07, 08, 09
             if (btnBoard07.Text == "X" && btnBoard08.Text == "X" && btnBoard09.Text == "X")
@@ -693,21 +717,23 @@ namespace NewTicTacToe
                 btnBoard09.Enabled = false;
             }
         }
-
         private void btnBoard09_Click(object sender, EventArgs e)
         {
             if (opponent)
             {
                 btnBoard09.Text = "X";
                 opponent = !opponent;
+                opponent_count++;
                 btnBoard09.Text = "X"; btnBoard09.Enabled = false;
             }
             else
             {
                 btnBoard09.Text = "O";
                 opponent = !opponent;
+                opponent_count++;
                 btnBoard09.Text = "O"; btnBoard09.Enabled = false;
             }
+            DrawChecker();
             //Checking of Winner for 9th button
             //Winner in Vertical = btnBoard 03, 06, 09
             if (btnBoard03.Text == "X" && btnBoard06.Text == "X" && btnBoard09.Text == "X")
@@ -790,13 +816,12 @@ namespace NewTicTacToe
                 btnBoard08.Enabled = false;
                 btnBoard09.Enabled = false;
             }
+            
         }
-
         private void btnBoardAll_Click(object sender, EventArgs e)
         {
-
+                
         }
-
         private void GameInfo_Click(object sender, EventArgs e)
         {
             MessageBox.Show("RULES FOR TIC-TAC-TOE.                                                                               -The game is played on a grid that's 3 squares by 3 squares.                      -You are X, and the other player is O.                                                         -The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner. ");
@@ -805,6 +830,5 @@ namespace NewTicTacToe
         {
             
         }
-
     }
 }
